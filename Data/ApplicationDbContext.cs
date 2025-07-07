092afdfb12765cdafaad72234c6cc92ab45bdef8
FATHIMA BEEVI <beevifathima175@gmail.com>
2025-07-07 15:47:01 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿// File: HMS.Web/Data/ApplicationDbContext.cs
+
+using HMS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HMS.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // This DbSet will become a table in our database named "Patients"
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
---
+++ Models/Patients.cs
@@ -0,0 +1,32 @@
+﻿
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HMS.Models
+{
+    public class Patient
+    {
+        [Key] // Marks this property as the primary key
+        public int PatientId { get; set; }
+
+        [Required(ErrorMessage = "Patient name is required.")]
+        [StringLength(100)]
+        [DisplayName("Patient Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayName("Date of Birth")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string Gender { get; set; } // Could be "Male", "Female", "Other"
+
+        [Required(ErrorMessage = "Contact number is required.")]
+        [DisplayName("Contact Number")]
+        [Phone]
+        public string ContactNumber { get; set; }
+
+        public string? Address { get; set; } // The '?' makes it an optional field
+    }
+}
